140f1380c567905b099837af455dfa18280ee855
shivasj <shivasj@gmail.com>
2025-11-10 13:12:52 -0500

    Source code + test files

---
+++ src/api-service/api/utils.py
@@ -0,0 +1,9 @@
+"""
+Utility functions for mathematical operations
+Used by the Cheese App API Service
+"""
+
+
+def power(base, exponent):
+    """Calculate base raised to the power of exponent"""
+    return base**exponent
